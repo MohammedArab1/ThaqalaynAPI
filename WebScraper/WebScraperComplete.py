4c7a0f27fe0e8ab11f8668cf9647b6d56171185a
MohammedArab1 <arab0033@algonquinlive.com>
2022-10-26 19:21:51 -0400

    Refining web scraper

--- WebScraper/WebScraperComplete.py
+++ WebScraper/WebScraperComplete.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import json
 from pathlib import Path
 import os
+import requests
 bookNameTranslations = [
     {"arabicName":"Al-Amālī","englishName":"Al-Amali"},
     {"arabicName":"Al-Khiṣāl","englishName":"Al-Khisal"},
@@ -26,70 +27,92 @@ bookNameTranslations = [
     {"arabicName":"Ṣifāt al-Shīʿa","englishName":"Sifat-Al-Shia"}
 ]
 
-URL = "https://thaqalayn.net/"
-mainPage = requests.get(URL)
-mainPageSoup = BeautifulSoup(mainPage.content, "html.parser")
-mainPageResults = mainPageSoup.find(id="library")
-absolute_path = os.path.dirname(__file__)
-for bookLink in mainPageResults.find_all('a'):
-    bookPage = requests.get(bookLink.get('href'))
-    bookSoup = BeautifulSoup(bookPage.content, "html.parser")
-    bookPageResults = bookSoup.find(id="content")
-    bookPageTitle = bookSoup.find("h1").get_text() #this will store the title of the book
-    bookPageAuthor = bookSoup.find("h6").get_text()
-    hadithsArray=[]
-    counter = 1
-    chapterName=""
-    for hadithlink in bookPageResults.find_all('a'):
-        hadithPage = requests.get(hadithlink.get('href'))
-        if hadithlink.find('span') != None:
-            chapterName = hadithlink.find('span').get_text().strip() #retrieves the chapter name if stored in <span>
-        if hadithlink.find('strong') != None:
-            chapterName = hadithlink.find('strong').get_text().strip() #retrieves the chapter name if stored in <strong>
-        hadithPageSoup = BeautifulSoup(hadithPage.content, "html.parser")
-        hadithPageResults = hadithPageSoup.find_all("div",class_="card text-center") #returns an array containing all the hadiths of this particular chapter. Could be 1 hadith, or many.
-        for hadith in hadithPageResults: 
-            englishText = hadith.find_all("p", class_="card-texts text-start")[0].get_text() #this takes a hadith, finds all elements 'p' with particular class (english text), and gets the text for that element
-            arabicText = hadith.find_all("p", class_="card-texts text-end libAr")[0].get_text()
-            pElement = hadith.find_all('p') #this simply gets all 'p' elements for any particular hadith
-            majlisiGrading = ""
-            behdudiGrading = ""
-            mohseniGrading = ""
-            for element in pElement:
-                if "Allamah Baqir al-Majlisi:" in element.get_text(): #if a majlisi grading is found, store it in variable.
-                    majlisiGrading = element.get_text()
-                if "Shaykh Baqir al-Behbudi:" in element.get_text(): #if a behdudi grading is found, store it in a variable.
-                    behdudiGrading = element.get_text()
-                if "Shaykh Asif al-Mohseni:" in element.get_text(): #if a mohseni grading is found, store it in a variable.
-                    mohseniGrading = element.get_text()
-            hadithPageUrl = hadith.find_all('a', class_="btn btn-primary")[0].get('href')
-            for element in bookNameTranslations:
-                if element['arabicName'] == bookPageTitle:
-                    bookPageTitle = element['englishName']
-            hadithObject = {
-                "id" : counter,
-                "book" : bookPageTitle,
-                "chapter" : chapterName,
-                "englishText" : englishText,
-                "arabicText" : arabicText,
-                "majlisiGrading" : majlisiGrading,
-                "behdudiGrading" : behdudiGrading,
-                "mohseniGrading" : mohseniGrading,
-                "URL" : hadithPageUrl
-            }
-            counter += 1
-            hadithsArray.append(hadithObject)
-    if "Ṭūsī" in bookPageAuthor:
-        savePath = os.path.join(absolute_path,bookPageTitle+"-Tusi.json")
-        with open(savePath, 'w', encoding='utf8') as json_file:
-            json.dump(hadithsArray, json_file, ensure_ascii=False)
-    elif "Nuʿmānī" in bookPageAuthor:
-        savePath = os.path.join(absolute_path,bookPageTitle+"-numani.json")
-        with open(savePath, 'w', encoding='utf8') as json_file:
-            json.dump(hadithsArray, json_file, ensure_ascii=False)
+#The following code creates a json with all the books, and with the min and max ranges.
+allBooksJson = []
+count = 1
+for element in bookNameTranslations:
+    if element["englishName"] == "Kitab-Al-Ghayba":
+        r =requests.get('https://thaqalayn-api.adaptable.app/api/Kitab-Al-Ghayba-numani')
+        array = json.loads(r.text)
+        newBookObject = {"id":count,"BookName":element["englishName"]+"-numani","idRangeMin":1,"idRangeMax":len(array)}
+        allBooksJson.append(newBookObject)
+        count+=1
+        r =requests.get('https://thaqalayn-api.adaptable.app/api/Kitab-Al-Ghayba-Tusi')
+        array = json.loads(r.text)
+        newBookObject = {"id":count,"BookName":element["englishName"]+"-Tusi","idRangeMin":1,"idRangeMax":len(array)}
     else:
-        with open(bookPageTitle+".json", 'w', encoding='utf8') as json_file:
-            json.dump(hadithsArray, json_file, ensure_ascii=False)
+        r =requests.get('https://thaqalayn-api.adaptable.app/api/'+element["englishName"])
+        array = json.loads(r.text)
+        newBookObject = {"id":count,"BookName":element["englishName"],"idRangeMin":1,"idRangeMax":len(array)}
+    allBooksJson.append(newBookObject)
+    count+=1
+with open("allBooks"+".json", 'w', encoding='utf8') as json_file:
+    json.dump(allBooksJson, json_file, ensure_ascii=False)
+
+# URL = "https://thaqalayn.net/"
+# mainPage = requests.get(URL)
+# mainPageSoup = BeautifulSoup(mainPage.content, "html.parser")
+# mainPageResults = mainPageSoup.find(id="library")
+# absolute_path = os.path.dirname(__file__)
+# for bookLink in mainPageResults.find_all('a'):
+#     bookPage = requests.get(bookLink.get('href'))
+#     bookSoup = BeautifulSoup(bookPage.content, "html.parser")
+#     bookPageResults = bookSoup.find(id="content")
+#     bookPageTitle = bookSoup.find("h1").get_text() #this will store the title of the book
+#     bookPageAuthor = bookSoup.find("h6").get_text()
+#     hadithsArray=[]
+#     counter = 1
+#     chapterName=""
+#     for hadithlink in bookPageResults.find_all('a'):
+#         hadithPage = requests.get(hadithlink.get('href'))
+#         if hadithlink.find('span') != None:
+#             chapterName = hadithlink.find('span').get_text().strip() #retrieves the chapter name if stored in <span>
+#         if hadithlink.find('strong') != None:
+#             chapterName = hadithlink.find('strong').get_text().strip() #retrieves the chapter name if stored in <strong>
+#         hadithPageSoup = BeautifulSoup(hadithPage.content, "html.parser")
+#         hadithPageResults = hadithPageSoup.find_all("div",class_="card text-center") #returns an array containing all the hadiths of this particular chapter. Could be 1 hadith, or many.
+#         for hadith in hadithPageResults: 
+#             englishText = hadith.find_all("p", class_="card-texts text-start")[0].get_text() #this takes a hadith, finds all elements 'p' with particular class (english text), and gets the text for that element
+#             arabicText = hadith.find_all("p", class_="card-texts text-end libAr")[0].get_text()
+#             pElement = hadith.find_all('p') #this simply gets all 'p' elements for any particular hadith
+#             majlisiGrading = ""
+#             behdudiGrading = ""
+#             mohseniGrading = ""
+#             for element in pElement:
+#                 if "Allamah Baqir al-Majlisi:" in element.get_text(): #if a majlisi grading is found, store it in variable.
+#                     majlisiGrading = element.get_text()
+#                 if "Shaykh Baqir al-Behbudi:" in element.get_text(): #if a behdudi grading is found, store it in a variable.
+#                     behdudiGrading = element.get_text()
+#                 if "Shaykh Asif al-Mohseni:" in element.get_text(): #if a mohseni grading is found, store it in a variable.
+#                     mohseniGrading = element.get_text()
+#             hadithPageUrl = hadith.find_all('a', class_="btn btn-primary")[0].get('href')
+#             for element in bookNameTranslations:
+#                 if element['arabicName'] == bookPageTitle:
+#                     bookPageTitle = element['englishName']
+#             hadithObject = {
+#                 "id" : counter,
+#                 "book" : bookPageTitle,
+#                 "chapter" : chapterName,
+#                 "englishText" : englishText,
+#                 "arabicText" : arabicText,
+#                 "majlisiGrading" : majlisiGrading,
+#                 "behdudiGrading" : behdudiGrading,
+#                 "mohseniGrading" : mohseniGrading,
+#                 "URL" : hadithPageUrl
+#             }
+#             counter += 1
+#             hadithsArray.append(hadithObject)
+#     if "Ṭūsī" in bookPageAuthor:
+#         savePath = os.path.join(absolute_path,bookPageTitle+"-Tusi.json")
+#         with open(savePath, 'w', encoding='utf8') as json_file:
+#             json.dump(hadithsArray, json_file, ensure_ascii=False)
+#     elif "Nuʿmānī" in bookPageAuthor:
+#         savePath = os.path.join(absolute_path,bookPageTitle+"-numani.json")
+#         with open(savePath, 'w', encoding='utf8') as json_file:
+#             json.dump(hadithsArray, json_file, ensure_ascii=False)
+#     else:
+#         with open(bookPageTitle+".json", 'w', encoding='utf8') as json_file:
+#             json.dump(hadithsArray, json_file, ensure_ascii=False)
 
 
 

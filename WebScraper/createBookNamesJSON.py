3022b4104350ec73191ffbd32c20b4ae30aae4a6
Mohammed Arab <arab0033@algonquinlive.com>
2022-10-26 22:24:26 -0400

    createBookNamesJSON.py is now finished, all json data is now updated. Now anytime data on the thaqalayn website changes, we can regenerate the data automatically and publish it to the DB to reflect the changes.

--- WebScraper/createBookNamesJSON.py
+++ WebScraper/createBookNamesJSON.py
@@ -9,22 +9,39 @@ from unidecode import unidecode
 
 #The following code creates a json with all the books, and with the min and max ranges.
 # allBooksJson = []
-# count = 1
-# for element in bookNameTranslations:
-#     if element["englishName"] == "Kitab-Al-Ghayba":
-#         r =requests.get('https://thaqalayn-api.adaptable.app/api/Kitab-Al-Ghayba-numani')
-#         array = json.loads(r.text)
-#         newBookObject = {"id":count,"BookName":element["englishName"]+"-numani","idRangeMin":1,"idRangeMax":len(array)}
-#         allBooksJson.append(newBookObject)
-#         count+=1
-#         r =requests.get('https://thaqalayn-api.adaptable.app/api/Kitab-Al-Ghayba-Tusi')
-#         array = json.loads(r.text)
-#         newBookObject = {"id":count,"BookName":element["englishName"]+"-Tusi","idRangeMin":1,"idRangeMax":len(array)}
-#     else:
-#         r =requests.get('https://thaqalayn-api.adaptable.app/api/'+element["englishName"])
-#         array = json.loads(r.text)
-#         newBookObject = {"id":count,"BookName":element["englishName"],"idRangeMin":1,"idRangeMax":len(array)}
-#     allBooksJson.append(newBookObject)
-#     count+=1
-# with open("allBooks"+".json", 'w', encoding='utf8') as json_file:
-#     json.dump(allBooksJson, json_file, ensure_ascii=False)
+
+URL = "https://thaqalayn.net/"
+mainPage = requests.get(URL)
+mainPageSoup = BeautifulSoup(mainPage.content, "html.parser")
+mainPageResults = mainPageSoup.find(id="library")
+absolute_path = os.path.dirname(__file__)
+allBooksJson = []
+count = 1
+for bookLink in mainPageResults.find_all('a'):
+    bookPage = requests.get(bookLink.get('href'))
+    bookSoup = BeautifulSoup(bookPage.content, "html.parser")
+    bookPageResults = bookSoup.find(id="content")
+    bookPageTitle = bookSoup.find("h1").get_text() #this will store the title of the book
+    bookPageTitle = unidecode(bookPageTitle)
+    bookPageTitle=bookPageTitle.replace(" ", "-")
+    bookPageTitle=bookPageTitle.replace("--","")
+    bookPageTitle=bookPageTitle.replace("`","")
+    bookPageTitle=bookPageTitle.replace("'","")
+    if bookPageTitle == "Kitab-Al-Ghayba":
+        r =requests.get('https://thaqalayn-ciqwid0uu-mohammedarab1.vercel.app/api/Kitab-al-Ghayba-numani')
+        array = json.loads(r.text)
+        newBookObject = {"id":count,"BookName":bookPageTitle+"-Numani","idRangeMin":1,"idRangeMax":len(array)}
+        allBooksJson.append(newBookObject)
+        count+=1
+        r =requests.get('https://thaqalayn-ciqwid0uu-mohammedarab1.vercel.app/api/Kitab-al-Ghayba-Tusi')
+        array = json.loads(r.text)
+        newBookObject = {"id":count,"BookName":bookPageTitle+"-Tusi","idRangeMin":1,"idRangeMax":len(array)}
+    else:
+        r =requests.get('https://thaqalayn-ciqwid0uu-mohammedarab1.vercel.app/api/'+bookPageTitle)
+        array = json.loads(r.text)
+        newBookObject = {"id":count,"BookName":bookPageTitle,"idRangeMin":1,"idRangeMax":len(array)}
+    allBooksJson.append(newBookObject)
+    count+=1  
+
+with open("BookNames"+".json", 'w', encoding='utf8') as json_file:
+    json.dump(allBooksJson, json_file, ensure_ascii=False)

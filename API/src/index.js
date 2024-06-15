c068655470f5a42ad37e3f7a00ba6c755316b695
MohammedArab1 <arab0033@algonquinlive.com>
2024-06-15 17:48:59 -0400

    modify index page

--- API/src/index.js
+++ API/src/index.js
@@ -31,9 +31,9 @@ app.get("/", (req, res) => {
     `<h1>Welcome to my REST API for thaqalayn.net</h1>
     <h2>Please visit <a href='https://github.com/MohammedArab1/ThaqalaynAPI'>my github page</a> for instructions on how the API works.</h2>
     </br> <h2>Visit the <a href='https://www.thaqalayn-api.net/api-docs'>SwaggerUI interface</a> to see all the API endpoints. </h2>
+    </br> <h2>GraphQL API: <a href='https://www.thaqalayn-api.net/graphql'>https://www.thaqalayn-api.net/graphql</a></h2> 
     </br> <h3>view all books to query from (v1 - old): <a href='https://www.thaqalayn-api.net/api/allbooks'>https://www.thaqalayn-api.net/api/allbooks</a></h3>   
     </br> <h3>view all books to query from (v2): <a href='https://www.thaqalayn-api.net/api/v2/allbooks'>https://www.thaqalayn-api.net/api/v2/allbooks</a></h3>  
-    </br> <h3>GraphQL API: <a href='https://www.thaqalayn-api.net/graphql'>https://www.thaqalayn-api.net/graphql</a></h3> 
     `,
   );
 });

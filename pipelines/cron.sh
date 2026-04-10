9906c90c7cf614dc88a8956be592931427710f58
mohammedarab1 <mohammed.arab.1881@gmail.com>
2026-04-10 12:59:43 -0400

    update cron

--- pipelines/cron.sh
+++ pipelines/cron.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+# Cron runs with a minimal environment. Pull runtime env injected by Docker
+# from PID 1 (cron daemon), then export only what this pipeline needs.
+if [ -r /proc/1/environ ]; then
+	eval "$(tr '\0' '\n' < /proc/1/environ | grep -E '^(WEBAPP_URL|MONGODB_URI|GEMINI_API_KEY)=' | sed 's/^/export /')"
+fi
+
 cd /app/V2
 
 make scrape_all datapath=./ThaqalaynData
@@ -14,4 +20,3 @@ make scrape_all datapath=./ThaqalaynData
 #   git commit -m "Automated data update"
 #   git push
 # fi
-EOF

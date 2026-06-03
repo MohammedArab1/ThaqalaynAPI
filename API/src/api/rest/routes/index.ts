f32bcfb1e8b27589a4d43fa5e085802da80e6800
mohammedarab1 <mohammed.arab.1881@gmail.com>
2026-06-02 20:51:25 -0400

    update homepage

--- API/src/api/rest/routes/index.ts
+++ API/src/api/rest/routes/index.ts
@@ -4,22 +4,29 @@ import {
 	type Request,
 	type Response,
 } from 'express';
+import path from 'path';
 import v1Router from './v1/hadith.js';
 import v2Router from './v2/hadith.js';
 
 const router = Router();
 
 router.get('/', (_req: Request, res: Response) => {
-	res.send(
-		`<h1>Welcome to my REST API for thaqalayn.net</h1>
-		<h2>Please visit <a href='https://github.com/MohammedArab1/ThaqalaynAPI'>my github page</a> for instructions on how the API works.</h2>
-		</br> <h2>Visit the <a href='https://www.thaqalayn-api.net/api-docs'>SwaggerUI interface</a> to see all the API endpoints. </h2>
-		</br> <h2>GraphQL API: <a href='https://www.thaqalayn-api.net/graphql'>https://www.thaqalayn-api.net/graphql</a></h2>
-		</br> <h3>view all books to query from (v1 - old): <a href='https://www.thaqalayn-api.net/api/allbooks'>https://www.thaqalayn-api.net/api/allbooks</a></h3>
-		</br> <h3>view all books to query from (v2): <a href='https://www.thaqalayn-api.net/api/v2/allbooks'>https://www.thaqalayn-api.net/api/v2/allbooks</a></h3>
-		</br> <h3>view halal/haram ingredients fetched from <a href='https://al-m.ca/halalguide/'>Al-maarif.com</a> (v2): <a href='https://www.thaqalayn-api.net/api/v2/ingredients'>https://www.thaqalayn-api.net/api/v2/ingredients</a></h3>
-		`,
+	const filePath = path.join(
+		process.cwd(),
+		'API',
+		'src',
+		'api',
+		'rest',
+		'public',
+		'index.html',
 	);
+
+	res.sendFile(filePath, (err) => {
+		if (err) {
+			console.error('Error sending index.html:', err);
+			res.status(500).send('Internal server error');
+		}
+	});
 });
 
 router.use('/api/v2', v2Router);
